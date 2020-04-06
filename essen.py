72fac95e99141def33d601cb0e4d0b36c6dc09dc
AgustinPardo <agu_pardo@hotmail.com>
2020-04-06 16:35:36 -0300

    DeJesus data analisis

--- essen.py
+++ essen.py
@@ -17,11 +17,9 @@ from matplotlib_venn import venn2
 import json
 import seaborn as sns
 
-
-
 def essen_test(model_tb, dic_return, dataset_name, dataset_excel, growth_thresh_mult):
     
-    model = model_tb.copy()
+    model = model_tb
     
     fal_pos_dic, fal_neg_dic = {}, {}
     true_neg_dic, true_pos_dic = {}, {}
@@ -32,10 +30,7 @@ def essen_test(model_tb, dic_return, dataset_name, dataset_excel, growth_thresh_
 
     growth_rates["genes"]=indexes
 
-    print ("Optimal growth", model.optimize().objective_value)    
-
     orig_growth_thres = growth_thresh_mult*model.optimize().objective_value
-    print ("Threshold growth", orig_growth_thres)
     
     true_pos, true_neg, fal_pos, fal_neg = 0, 0, 0, 0
     
@@ -43,9 +38,9 @@ def essen_test(model_tb, dic_return, dataset_name, dataset_excel, growth_thresh_
     grif_thres = 0.1
         
     for index, row in dataset_excel.iterrows():
-        gene = str(row["Locus"])
         
         if dataset_name == "griffin":
+            gene = str(row["Locus"])
             try:
                 growth=growth_rates[growth_rates.genes.isin([gene])].growth[0]                            
                 try:                    
@@ -55,18 +50,22 @@ def essen_test(model_tb, dic_return, dataset_name, dataset_excel, growth_thresh_
                         true_pos_dic.update({gene: [growth, float(row["p value"])]})
 
                     # False Positive - predicts that it grows (not essential) when it actually essential
-                    if float(row["p value"]) < grif_thres and growth > orig_growth_thres:
+                    elif float(row["p value"]) < grif_thres and growth > orig_growth_thres:
                         fal_pos = fal_pos + 1
                         fal_pos_dic.update({gene: [growth, float(row["p value"])]})
                     
                     # True Negative - predicts that the gene is essential (no growth) and is correct
-                    if float(row["p value"]) < grif_thres and growth < orig_growth_thres:
+                    elif float(row["p value"]) < grif_thres and growth < orig_growth_thres:
                         true_neg = true_neg + 1
                         true_neg_dic.update({gene: [growth, float(row["p value"])]})
 
-                    if float(row["p value"]) > grif_thres and growth < orig_growth_thres:
+                    elif float(row["p value"]) > grif_thres and growth < orig_growth_thres:
                         fal_neg = fal_neg + 1
-                        fal_neg_dic.update({gene: [growth, float(row["p value"])]})                    
+                        fal_neg_dic.update({gene: [growth, float(row["p value"])]})    
+                    else:
+                        # Hay algunos que tiene growth = nan
+                        print(str(gene))
+                
                         
                 except:
                     pass
@@ -81,6 +80,7 @@ def essen_test(model_tb, dic_return, dataset_name, dataset_excel, growth_thresh_
                     # print("Ese gen no esta en el modelo: "+str(gene))
                         
         elif dataset_name == "loerger":
+            gene = str(row["ORF ID"])
             """
             ES being near 0
             NE being near the mean
@@ -88,31 +88,39 @@ def essen_test(model_tb, dic_return, dataset_name, dataset_excel, growth_thresh_
             GA 5 times the mean
             """
             try:
-                growth = growth_rates.loc[gene, "flux"]
-                
-                # TP
-                if (row["Final Call"] == "NE" or row["Final Call"] == "GA") and growth > orig_growth_thres:
-                    true_pos = true_pos + 1
-                    true_pos_dic.update({gene: [growth]})
-                        
-                # FN
-                if (row["Final Call"] == "NE" or row["Final Call"] == "GA") and growth < orig_growth_thres:
-                    fal_neg = fal_neg + 1
-                    fal_neg_dic.update({gene: [growth]})
-                        
-                # TN
-                if (row["Final Call"] == "ES" or row["Final Call"] == "ESD" or row["Final Call"] == "GD") and growth < orig_growth_thres:
-                # if (row["Final Call"] == "ES") and growth < orig_growth_thres:
-                    true_neg = true_neg + 1
-                    true_neg_dic.update({gene: [growth]})
-                        
-                # FP
-                if (row["Final Call"] == "ES" or row["Final Call"] == "ESD" or row["Final Call"] == "GD") and growth > orig_growth_thres:
-                # if (row["Final Call"] == "ES") and growth > orig_growth_thres:
-                    fal_pos = fal_pos + 1
-                    fal_pos_dic.update({gene: [growth]})
+                growth=growth_rates[growth_rates.genes.isin([gene])].growth[0] 
+                # growth = growth_rates.loc[gene, "flux"]
+                try:           
+                    # TP
+                    if (row["Final Call"] == "NE" or row["Final Call"] == "GA") and growth > orig_growth_thres:
+                        true_pos = true_pos + 1
+                        true_pos_dic.update({gene: [growth]})
+                           
+                    # FN
+                    elif (row["Final Call"] == "NE" or row["Final Call"] == "GA") and growth < orig_growth_thres:
+                        fal_neg = fal_neg + 1
+                        fal_neg_dic.update({gene: [growth]})
+                            
+                    # TN
+                    elif (row["Final Call"] == "ES" or row["Final Call"] == "ESD" or row["Final Call"] == "GD") and growth < orig_growth_thres:
+                    # if (row["Final Call"] == "ES") and growth < orig_growth_thres:
+                        true_neg = true_neg + 1
+                        true_neg_dic.update({gene: [growth]})
+                            
+                    # FP
+                    elif (row["Final Call"] == "ES" or row["Final Call"] == "ESD" or row["Final Call"] == "GD") and growth > orig_growth_thres:
+                    # if (row["Final Call"] == "ES") and growth > orig_growth_thres:
+                        fal_pos = fal_pos + 1
+                        fal_pos_dic.update({gene: [growth]})
+                    else:
+                        # Hay algunos que tiene growth = nan
+                        print(str(gene))
+                except:
+                    pass
+                    # print("algo mas rompio a loerger")
             except:
                 pass
+                # print("Este gen no esta en el mdoelo: "+str(gene))
                 
             
     # ---Analyze and Print results ---
@@ -132,15 +140,20 @@ def essen_test(model_tb, dic_return, dataset_name, dataset_excel, growth_thresh_
         return fal_neg_dic, fal_pos_dic
     elif dic_return == "Yes both":
         return fal_neg_dic, fal_pos_dic, true_neg_dic, true_pos_dic
-    
-griffin_file = "/home/agustin/workspace/Paper_IEK1011/griffin/ppat.1002251.s002.xlsx"   
-griffin_excel = pd.read_excel(griffin_file,skiprows = 9,keep_default_na=False)
+ 
 
 #No esta disponible
 #model_iEK.solver = "gurobi"
 
-# model_m7H10_file_path="/home/agustin/workspace/Paper_IEK1011/supplementaryMaterial/12918_2018_557_MOESM3_ESM/iEK1011_m7H10_media.json"
-# model_iEK_m7H10 = cobra.io.load_json_model(model_m7H10_file_path)
+
+loerger_file = "/home/agustin/workspace/Paper_IEK1011/DeJesus/mbo002173137st3.xlsx"   
+loerger_excel = pd.read_excel(loerger_file,skiprows = 1,keep_default_na=False)
+
+griffin_file = "/home/agustin/workspace/Paper_IEK1011/griffin/ppat.1002251.s002.xlsx"   
+griffin_excel = pd.read_excel(griffin_file,skiprows = 9,keep_default_na=False)
+
+model_loerger_file_path="/home/agustin/workspace/Paper_IEK1011/supplementaryMaterial/12918_2018_557_MOESM3_ESM/iEK1011_deJesusEssen_media.json"
+model_iEK_loerger = cobra.io.load_json_model(model_loerger_file_path)
 
 model_griffin_file_path="/home/agustin/workspace/Paper_IEK1011/supplementaryMaterial/12918_2018_557_MOESM3_ESM/iEK1011_griffinEssen_media.json"
 model_iEK_griffin = cobra.io.load_json_model(model_griffin_file_path)
@@ -150,3 +163,6 @@ grow_thresh = 0.2
 print ("iEK1011_griffin")
 print(model_iEK_griffin.solver.configuration)
 FN_dic_iEK, FP_dic_iEK, TN_dic_iEK, TP_dic_iEK = essen_test(model_iEK_griffin, "Yes both", "griffin", griffin_excel, grow_thresh)
+print ("iEK1011_loerger")
+print(model_iEK_loerger.solver.configuration)
+FN_dic_iEK, FP_dic_iEK, TN_dic_iEK, TP_dic_iEK = essen_test(model_iEK_loerger, "Yes both", "loerger", loerger_excel, grow_thresh)
